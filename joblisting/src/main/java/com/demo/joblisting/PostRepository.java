13d5bc839181ea7d16f9a97b1f2f5a6244657d2a
Sagarika <sagie_tech>
2023-10-03 15:42:46 +0530

    PostRepository.java deleted

--- joblisting/src/main/java/com/demo/joblisting/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.joblisting;
-
-import com.demo.joblisting.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post,String> {
-}
